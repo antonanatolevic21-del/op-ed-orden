753e78093c9f918f1c4743b17ee3222041822580
antonanatolevic21-del <antonanatolevic21@gmail.com>
2026-07-23 22:57:32 +0300

    Refresh album card styles

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260723-track-panel-compact5';
+  const version = '20260723-track-panel-compact6';
 
   const albumStylesheet = document.createElement('link');
   albumStylesheet.rel = 'stylesheet';
@@ -21,7 +21,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register('./sw.js?v=20260723-force6', {
+      const registration = await navigator.serviceWorker.register('./sw.js?v=20260723-force7', {
         updateViaCache: 'none'
       });
       await registration.update();
