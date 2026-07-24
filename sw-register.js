7cb4ad6f9c5cafa314943cb573d75d7508a3c470
antonanatolevic21-del <antonanatolevic21@gmail.com>
2026-07-24 20:17:11 +0300

    Refresh interactive stats rows

--- sw-register.js
+++ sw-register.js
@@ -5,7 +5,7 @@
   const profileFiltersVersion = '20260724-profile-filters2';
   const filterChipsVersion = '20260724-filter-chips2';
   const deepLinksVersion = '20260724-deep-links1';
-  const statsLiteVersion = '20260724-stats-lite1';
+  const statsLiteVersion = '20260724-stats-lite2';
 
   const filterUiStylesheet = document.createElement('link');
   filterUiStylesheet.rel = 'stylesheet';
