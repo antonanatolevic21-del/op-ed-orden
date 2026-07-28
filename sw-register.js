88032551a5db082c3fcb5767ee45a655769236dc
antonanatolevic21-del <antonanatolevic21@gmail.com>
2026-07-28 21:13:51 +0300

    Prevent password prompt after rating

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
 (() => {
-  const primaryVersion = '20260728-rating-no-refresh1';
+  const primaryVersion = '20260728-rating-no-password1';
   const seasonFillVersion = '20260725-season-fill2';
   const manualTopInsertVersion = '20260726-manual-top-insert13';
   const manualTopInsertFixVersion = '20260725-manual-top-insert-fix4';
