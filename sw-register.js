66a68d36722f987f7bb5a36f6d2e3a14a1552889
antonanatolevic21-del <antonanatolevic21@gmail.com>
2026-08-03 12:19:27 +0300

    Refresh compact status controls

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
 (() => {
-  const primaryVersion = '20260803-hotkey-skip-status2';
+  const primaryVersion = '20260803-status-buttons-stack1';
   const discoveryVersion = '20260731-duel-editor-draft1';
   const tasteComparisonVersion = '20260731-taste-account-fix1';
   const imageUploadSecretVersion = '20260731-image-secret-memory1';
